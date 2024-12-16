a4bfe77ecf945f415fb0d17b83db5b428075c83a
Abd Al-Gader Sirag <abdalgader.dev@gmail.com>
2024-12-16 02:05:42 -0800

    create filament basic scafold

---
+++ app/Filament/Resources/DetailResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DetailResource\Pages;
+use App\Filament\Resources\DetailResource\RelationManagers;
+use App\Models\Detail;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DetailResource extends Resource
+{
+    protected static ?string $model = Detail::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Textarea::make('rooms')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('bathrooms')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('halls')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('floors')
+                    ->required(),
+                Forms\Components\Textarea::make('kitchen')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Select::make('house_id')
+                    ->relationship('house', 'title')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('floors')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('house.title')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDetails::route('/'),
+            'create' => Pages\CreateDetail::route('/create'),
+            'edit' => Pages\EditDetail::route('/{record}/edit'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Detail;
+use App\Models\House;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
+        Detail::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
