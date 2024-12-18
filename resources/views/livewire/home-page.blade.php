743b30b8a5e620096b8e5d94c300a4460e44fafc
Abd Al-Gader Sirag <abdalgader.dev@gmail.com>
2024-12-18 01:50:52 -0800

    add the filters to the index views

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,9 +1,95 @@
-
 <div class="flex w-full">
-    <div class="container w-10/12 mx-auto px-4 py-8">
-        <div class="mb-4">
-            <input type="search" live:model="search" name="search" id="search" class="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Search Estates">
+
+    <div class="w-3/12 mx-auto px-4 py-8 text-gray-700">
+        <input type="search" live:model="search" name="search" id="search" class="border border-gray-300 rounded-md w-full px-4 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Search Estates">
+
+        <div class="py-2 flex" style="flex-wrap: wrap">
+            <div class="flex mt-3">
+                <input wire:model="form.rent" type="checkbox" name="all" class="w-5 h-5 mr-1" checked>
+                <label for="all" class="mr-3">All</label>
+            </div>
+
+            @foreach (config('estate.type') as $type)
+                <div class="flex mt-3">
+                    <input wire:model="{{ $type }}" type="checkbox" name="{{ $type }}" class="w-5 h-5 mr-1">
+                    <label for="{{ $type }}" class="mr-3">{{ $type }}</label>
+                </div>
+            @endforeach
+
         </div>
+
+        <h4 class="my-2">Price :</h4>
+        <div class="flex mb-2">
+            <input type="text" placeholder="min price" class="border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200 w-1/2 mr-2">
+            <input type="text" placeholder="max price" class="border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200 w-1/2">
+        </div>
+
+        <div class="mb-2">
+            <input wire:model="rent" type="checkbox" name="rent" id="rent" class="mr-2">
+            <label for="rent">show for rent only</label>
+        </div>
+
+        <div class="flex justify-between">
+            <div class="w-1/2">
+                <h4 class="ml-1">rooms :</h4>
+                <div class="flex">
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 mr-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full rotate-90">
+                    </button>
+                    <span>1</span>
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 ml-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full -rotate-90">
+                    </button>
+                </div>
+            </div>
+
+            <div class="w-1/2">
+                <h4 class="ml-1">bathrooms :</h4>
+                <div class="flex">
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 mr-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full rotate-90">
+                    </button>
+                    <span>1</span>
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 ml-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full -rotate-90">
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="flex justify-between mt-4">
+            <div class="w-1/2">
+                <h4 class="ml-1">kitchens :</h4>
+                <div class="flex">
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 mr-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full rotate-90">
+                    </button>
+                    <span>1</span>
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 ml-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full -rotate-90">
+                    </button>
+                </div>
+            </div>
+
+            <div class="w-1/2">
+                <h4 class="ml-1">floors :</h4>
+                <div class="flex">
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 mr-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full rotate-90">
+                    </button>
+                    <span>1</span>
+                    <button class="border-2 border-gray-300 rounded-full w-7 h-7 ml-2">
+                        <img src="{{ asset('/images/arrow.svg') }}" class="rounded-full -rotate-90">
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+    <div class="container w-10/12 mx-auto px-4 py-8">
         <div>
             @forelse ($estates as $estate)
                 <div class="bg-white shadow-md rounded-xl mb-4">
@@ -25,8 +111,5 @@
                 <p class="text-center text-gray-500">There are no houses at the moment.</p>
             @endforelse
         </div>
-    </div>
-
-    <div class="w-3/12">
     </div>
 </div>
