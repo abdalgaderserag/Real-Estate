5cfb503f480f0c6f0810e3770709c49bcf8d3b9f
Abd Al-Gader Sirag <abdalgader.dev@gmail.com>
2025-01-14 22:48:42 -0800

    create the landing page structure

---
+++ resources/views/livewire/landing-page.blade.php
@@ -0,0 +1,191 @@
+<div class="flex w-full justify-center">
+    <div class="w-5/6">
+        <header class="header">
+            <nav class="nav bg-green-400">
+                <img src="{{ assets('images/index/logo.svg') }}">
+            </nav>
+            <div>
+                <div>
+                    <h1>BYTOONA</h1>
+                </div>
+                <div>
+                    <input type="search">
+                    <button>Search</button>
+                </div>
+            </div>
+        </header>
+
+        <section>
+            <h2>Find the house that suite you</h2>
+            <div>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <h3>homestead</h3>
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nulla, vel reiciendis.</div>
+                        <div>
+                            <div>
+                                <div>
+                                    <img src="" alt=""> 10
+                                </div>
+                                <div>
+                                    <img src="" alt=""> 2
+                                </div>
+                                <div>
+                                    <img src="" alt=""> 4
+                                </div>
+                            </div>
+                            <div>
+                                <button>Book</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <h3>homestead</h3>
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nulla, vel reiciendis.</div>
+                        <div>
+                            <div>
+                                <div>
+                                    <img src="" alt=""> 10
+                                </div>
+                                <div>
+                                    <img src="" alt=""> 2
+                                </div>
+                                <div>
+                                    <img src="" alt=""> 4
+                                </div>
+                            </div>
+                            <div>
+                                <button>Book</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <h3>homestead</h3>
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nulla, vel reiciendis.</div>
+                        <div>
+                            <div>
+                                <div>
+                                    <img src="" alt=""> 10
+                                </div>
+                                <div>
+                                    <img src="" alt=""> 2
+                                </div>
+                                <div>
+                                    <img src="" alt=""> 4
+                                </div>
+                            </div>
+                            <div>
+                                <button>Book</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div>
+                discover more...
+            </div>
+        </section>
+
+        <section>
+            <div>
+                <div></div>
+                <div>
+                    <img src="" alt="">
+                    <div><span>find your</span> dream house effortlessly</div>
+                </div>
+            </div>
+
+            <div>
+                <h2>Our features:</h2>
+                <div>
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>no fee's</h3>
+                            <div>free for use no fee's no subscription no anything!</div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>no fee's</h3>
+                            <div>free for use no fee's no subscription no anything!</div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>no fee's</h3>
+                            <div>free for use no fee's no subscription no anything!</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <section>
+            <div>
+                <div>
+                    <h3>About us:</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis deserunt vitae doloribus. Voluptates modi beatae ullam! Beatae eaque voluptate aliquam id, nihil vero culpa dolore optio nulla consequuntur odit neque.</p>
+                </div>
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+            <div></div>
+        </section>
+
+        <section>
+            <div>
+                <div class="social"></div>
+                <div>
+                    <img src="{{ assets('images/index/logo.svg')}}">
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li><h3>explore</h3></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><h3>explore</h3></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><h3>explore</h3></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                    <li><a href="#">home page</a></li>
+                </ul>
+            </div>
+        </section>
+
+
+        <footer>
+            this site created by <a href="#">Abdalgader m.sirag makki</a>
+        </footer>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use App\Livewire\Estate\CreateEstatePage;
 use App\Livewire\Estate\EditEstatePage;
 use App\Livewire\Estate\ShowEstatePage;
 use App\Livewire\HomePage;
+use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomePage::class)->name('estate.index');
+
+Route::get('/', LandingPage::class)->name('index');
+Route::get('/dashboard', HomePage::class)->name('estate.index');
 Route::get('/estate/{house}', ShowEstatePage::class)->name('estate.show');
 Route::get('/create/estate', CreateEstatePage::class)->name('estate.create');
 Route::get('/estate/{house}/edit', EditEstatePage::class)->name('estate.edit');
